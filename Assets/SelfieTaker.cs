b7c5b3780cc23247d46181e1fcd895df2854741a
Yang Wang <yang.wang.dec@gmail.com>
2019-05-21 15:36:39 -0400

    init for mark

---
+++ Assets/SelfieTaker.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+[RequireComponent(typeof(Camera))]
+public class SelfieTaker : MonoBehaviour
+{
+    [SerializeField] bool take;
+    [SerializeField] StuffMaker stuffMaker;
+
+    Camera cam;//camera used specifically for "selfie" taking
+    RenderTexture rt;
+    Texture2D frameBufferCapture;
+
+    int selfieId = 0;
+
+    public int captureWidth = 1000;
+    public int captureHeight = 1000;
+
+
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        //init this cam
+
+        rt = new RenderTexture(captureWidth, captureHeight, 32, RenderTextureFormat.ARGB32);
+        cam.targetTexture = rt;
+
+       // cam.enabled = false;
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test
+        if (take)
+        {
+            take = false;
+            var selected = stuffMaker.GetOne();
+
+            StartCoroutine(TakeOneSelfie(selected.transform));
+        }
+    }
+
+    //use coroutine to avoid performance impact
+    public IEnumerator TakeOneSelfie(Transform target, System.Action<string> onFinish = null)
+    {
+
+        cam.Render();
+
+        string filePath =  Application.dataPath + "/selfie" + (selfieId++) + ".jpg";
+
+
+
+        frameBufferCapture = new Texture2D(captureWidth, captureHeight, TextureFormat.RGB24, false, true);
+
+
+
+      //  cam.enabled = true;
+
+        //move camera to position. in the future maybe randomize the position a bit so each take is from a different angle?
+        transform.position = target.transform.position + new Vector3(0, -10, 0);
+        transform.LookAt(target);
+
+
+        RenderTexture.active = rt;
+        frameBufferCapture.ReadPixels(new Rect(0, 0, captureWidth, captureHeight), 0, 0);
+        RenderTexture.active = null;
+
+
+        try
+        {
+            //@Mark: you proabbly can do something different here to get the uri more directly
+            File.WriteAllBytes(filePath, frameBufferCapture.EncodeToJPG());
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e);
+            yield break;
+
+        }
+
+        if (onFinish != null)
+        {
+            onFinish(filePath);
+        }
+      //  cam.enabled = false;
+
+
+        yield break;
+
+
+    }
+
+
+
+
+
+
+
+
+}
---
+++ Assets/StuffMaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffMaker : MonoBehaviour
+{
+
+
+    public int n = 100;
+    public int r = 500;
+
+    public List<GameObject> things = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            GameObject newGO = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            things.Add(newGO);
+            newGO.transform.parent = this.transform;
+            newGO.transform.localScale = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+            newGO.transform.position = new Vector3(Random.Range(-r, r), Random.Range(-r, r), Random.Range(-r, r));
+        }
+    }
+
+    public GameObject GetOne()
+    {
+        if (things.Count == 0) return null;
+        return things[Random.Range(0, things.Count - 1)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
